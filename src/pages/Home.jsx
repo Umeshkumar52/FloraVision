bc315c3840b846bec2b9e42c8f42e2cb25ce7ad8
Umeshkumar52 <uk3993907@gmail.com>
2025-10-29 21:47:03 +0530

    update successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,10 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Product from './Product'
 import Footer from '../components/Footer'
-// import BgPlant from '../assets/BgPlant.png'
 export default function Home() {
   return (
       
-    <div className='bg-[#061406e1]/20'>
+    <div className='bg-[#061406e1]'>
     <div style={{  backgroundImage: `url(BgPlant.png)`}} className={`w-full h-full px-[3rem] flex gap-[5rem] text-[#c7c4c4] bg-center bg-no-repeat bg-cover flex-col`}>
     <Navbar/>
     <Header/>

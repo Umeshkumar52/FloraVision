a28f966eafd0ce9e74984ecad19c4cce9edccb46
Umeshkumar52 <uk3993907@gmail.com>
2025-10-29 21:03:21 +0530

    debug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,9 @@
-import React from "react";
 import { IoPlayCircleOutline } from "react-icons/io5";
-import { PiDotOutline } from "react-icons/pi";
-import star1 from "../assets/star1.png";
-import star5 from "../assets/star5.png";
 import { FaAngleRight } from "react-icons/fa6";
 import { PiDotOutlineFill } from "react-icons/pi";
-import { PiDotBold } from "react-icons/pi";
+
 import { MdHorizontalRule } from "react-icons/md";
-import Roonnie from "../assets/unsplash_3TLl_97HNJo.png";
+
 import Aglaonema from "../assets/Aglaonema.png";
 import trendPlant1 from "../assets/trendPlant1.png";
 import trendPlant2 from "../assets/trendPlant2.png";
@@ -18,7 +14,9 @@ export default function Header() {
       <div className="flex flex-col md:flex-row px-20 justify-center md:justify-between">
         <div className="flex md:items-start  items-center mt-8 flex-col gap-[6rem]">
           <div className="space-y-2">
-            <h1 className="md:text-8xl text-6xl font-semibold">Earth’s Exhale</h1>
+            <h1 className="md:text-8xl text-6xl font-semibold">
+              Earth’s Exhale
+            </h1>
             <p className="text-lg md:max-w-[60%] whitespace-pre-wrap break-words">
               "Earth Exhale" symbolizes the purity and vitality of the Earth's
               natural environment and its essential role in sustaining life.
@@ -29,7 +27,7 @@ export default function Header() {
               </button>
               <div className="flex gap-2 items-center">
                 <IoPlayCircleOutline className="text-5xl" />
-                <h6 style={{fontFamily:"Indie Flower"}}>Live demo...</h6>
+                <h6 style={{ fontFamily: "Indie Flower" }}>Live demo...</h6>
               </div>
             </div>
           </div>
@@ -43,13 +41,13 @@ export default function Header() {
               <div>
                 <h4>Roonie Hamill</h4>
                 <div className="flex">
-                {Array.from({ length: 4 }, (_, i) => i + 1).map(
-                  (item, index) => (
-                    <img src={star1} key={index} className="size-4" />
-                  )
-                )}
-                <img src={star5} className="size-4" />
-              </div>
+                  {Array.from({ length: 4 }, (_, i) => i + 1).map(
+                    (item, index) => (
+                      <img src="Star1.png" key={index} className="size-4" />
+                    )
+                  )}
+                  <img src="Star5.png" className="size-4" />
+                </div>
               </div>
             </div>
             <p>
@@ -58,32 +56,31 @@ export default function Header() {
             </p>
           </div>
         </div>
-        
-    
-          <div className="relative h-[29rem] border-2  border-slate-400 max-w-sm rounded-[5rem] flex bg-[#4d604d]/20 justify-center items-center ">
-            <img className="-mt-[8rem] z-50 max-w-lg " src={Aglaonema} />
-            <div className="absolute w-full z-50 flex flex-col gap-3 bottom-[2rem] space-y-2">
-              <div className="pl-16 pr-6">
-                <p>Indoor Plant </p>
-                <div className="space-y-3">
-                  <div className="w-full gap-14 flex items-center justify-between">
-                    <h1 className="text-2xl font-normal">Aglaonema plant</h1>
-                    <FaAngleRight />
-                  </div>
 
-                  <button className="py-2 border-2 rounded-xl px-10 text-xl">
-                    Buy Now
-                  </button>
+        <div className="relative h-[29rem] border-2  border-slate-400 max-w-sm rounded-[5rem] flex bg-[#4d604d]/20 justify-center items-center ">
+          <img className="-mt-[8rem] z-50 max-w-lg " src={Aglaonema} />
+          <div className="absolute w-full z-50 flex flex-col gap-3 bottom-[2rem] space-y-2">
+            <div className="pl-16 pr-6">
+              <p>Indoor Plant </p>
+              <div className="space-y-3">
+                <div className="w-full gap-14 flex items-center justify-between">
+                  <h1 className="text-2xl font-normal">Aglaonema plant</h1>
+                  <FaAngleRight />
                 </div>
-              </div>
-              <div className="w-full flex text-lg justify-center items-center">
-                <MdHorizontalRule />
-                <PiDotOutlineFill />
-                <PiDotOutlineFill />
+
+                <button className="py-2 border-2 rounded-xl px-10 text-xl">
+                  Buy Now
+                </button>
               </div>
             </div>
+            <div className="w-full flex text-lg justify-center items-center">
+              <MdHorizontalRule />
+              <PiDotOutlineFill />
+              <PiDotOutlineFill />
+            </div>
           </div>
-         
+        </div>
+
         {/* <div className="relative">
           <div className="flex items-center">
             <img className="z-50 max-w-3xl" src={Aglaonema} />
@@ -119,48 +116,48 @@ export default function Header() {
       </div>
       {/* trend plants */}
 
-        <div className="flex justify-center">
-          <div className="w-full flex max-w-[500px] px-6 mt-6">
-            <div className="relative ">
-              <h1 className="text-4xl px-8 leading-tight font-semibold ">
-               Our Trendy plants
-              </h1>
-              <svg
-                className="absolute left-0 top-0 -translate-y-4 -translate-x-2"
-                width="80"
-                height="60"
-                viewBox="0 0 80 60"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 40 V20 Q10 10 20 10 H60"
-                  stroke="#B8A83D"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+      <div className="flex justify-center">
+        <div className="w-full flex max-w-[500px] px-6 mt-6">
+          <div className="relative ">
+            <h1 className="text-4xl px-8 leading-tight font-semibold ">
+              Our Trendy plants
+            </h1>
+            <svg
+              className="absolute left-0 top-0 -translate-y-4 -translate-x-2"
+              width="80"
+              height="60"
+              viewBox="0 0 80 60"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 40 V20 Q10 10 20 10 H60"
+                stroke="#B8A83D"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
 
-              <svg
-                className="absolute pb-4 right-0 top-0 -translate-y-4 translate-x-2 rotate-180"
-                width="80"
-                height="80"
-                viewBox="0 0 80 60"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 40 V20 Q10 10 20 10 H60"
-                  stroke="#B8A83D"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
+            <svg
+              className="absolute pb-4 right-0 top-0 -translate-y-4 translate-x-2 rotate-180"
+              width="80"
+              height="80"
+              viewBox="0 0 80 60"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 40 V20 Q10 10 20 10 H60"
+                stroke="#B8A83D"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </div>
         </div>
+      </div>
 
       <div className="flex text-white flex-col gap-[8rem] px-[12rem]">
         <div className=" md:h-[26rem] w-full px-[6rem] py-20 md:py-0 flex flex-col md:flex-row justify-between items-center border-2 border-[#4d604d] bg-[#4d604d]/50 rounded-[151px]">
